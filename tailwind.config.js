0545da8da1960e6fe619ff6183b712829055008b
philrussel21 <phil.antiporda21@gmail.com>
2024-03-09 00:45:30 +1000

    chore: init Roboto font as default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line node/no-unpublished-require
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
