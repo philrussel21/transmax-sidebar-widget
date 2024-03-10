c083e14d9fbda40faaa6fc82dc8ab31b40dce837
philrussel21 <phil.antiporda21@gmail.com>
2024-03-10 21:46:45 +1000

    fix: update App layout and structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Widget from './components/partials/widget';
 
 const App = () => {
   return (
-    <div className="text-white/60 h-screen w-screen flex justify-center items-center bg-black/80">
-      <div className="max-w-xs mx-auto">
+    <div className="text-white/60 h-screen w-screen bg-black/80">
+      <div className="max-w-[260px] mx-auto pt-20">
         <Widget />
       </div>
     </div>
